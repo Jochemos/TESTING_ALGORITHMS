b2e6320427c457417eaca5cfc8508205810b1b5c
Jochemos <danjoch@onet.pl>
2022-06-13 14:05:40 +0200

    'new'

---
+++ sortMoreThanOne/sort.js
@@ -0,0 +1,20 @@
+const elements = `6434`;
+
+const replaceElements = (getElement) => {
+  const arrayElements = [];
+  for(let i=0; i<getElement.length; i++){
+    arrayElements.push(getElement[i])
+  }
+
+  return arrayElements;
+}
+
+const searchElements = () => {
+  const justItems = replaceElements(elements);
+  const testItems = [];
+
+
+
+}
+
+searchElements();
